62dd84e866928ac3c848aa30fdd6c893ba6f402c
Joshua Rhodes <jdw.rhodes@gmail.com>
2021-03-16 12:11:18 -0500

    Added imports

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using D3. d3.select is telling JS to look for the <tbody> tags in the HTML page
+var tbody = d3.select('tbody');
