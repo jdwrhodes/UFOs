87f613241aec4169fc8af3e27797fb954b91d5b9
Joshua Rhodes <jdw.rhodes@gmail.com>
2021-03-22 20:11:16 -0500

    updated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -33,10 +33,10 @@ function updateFilters() {
     let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
     let elementValue = changedElement.property("value");
-    console.log(elementValue);
+
     // 4c. Save the id of the filter that was changed as a variable.
     let filterId = changedElement.attr("id");
-    console.log(filterId);
+
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
@@ -47,23 +47,28 @@ function updateFilters() {
     }
   
     // 6. Call function to apply all filters and rebuild the table
-    filterTable(filterId);
+    filterTable();
   
-  }
+  };
   
   // 7. Use this function to filter the table when data is entered.
-  function filterTable(filter) {
+  function filterTable() {
   
     // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    filteredData = filteredData.filter(row => row[filter] === filters[filter]);
-  
+    // for (let filter in filters) {
+    // filteredData = filteredData.filter(row => row.filter === filter);
+    // };
+    Object.entries(filters).forEach(([key, value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+    });
+
     // 10. Finally, rebuild the table using the filtered data
-    buildTable(filteredData);
-  }
+    buildTable(filteredData)
+  };
   
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll('input').on('change', updateFilters);
@@ -71,8 +76,3 @@ function updateFilters() {
 
   // Build the table when the page loads
   buildTable(tableData);
-
-
-//Python version for the loop
-//for row in filteredData:
-// cleanData == 
